5b00283bab0380e46f72713db3f57baf4771298a
Your Name <zayatsmargo@gmail.com>
2021-12-16 18:04:44 +0200

    feat:modified hw16

--- src/main/java/com/company/zayats/hw16/Dictionary.java
+++ src/main/java/com/company/zayats/hw16/Dictionary.java
@@ -4,30 +4,27 @@ import java.util.*;
 
 public class Dictionary {
 
-    private Map<String, Integer> dictionary = new HashMap<>();
-
-    public Dictionary() {
-        dictionary.put("juice", 2);
-        dictionary.put("money", 5);
-        dictionary.put("car", 1);
-        dictionary.put("lion", 6);
-        dictionary.put("weather", 8);
-        System.out.println(dictionary);
-    }
-
-    public Map<String, Integer> sortDictionary() {
-        List<Map.Entry<String, Integer>> list = new ArrayList<>(dictionary.entrySet());
-        list.sort(Map.Entry.comparingByValue());
-
-        Map<String, Integer> result = new LinkedHashMap<>();
-        for (Map.Entry<String, Integer> entry : list) {
-            result.put(entry.getKey(), entry.getValue());
-        }
-        return result;
-    }
-
     public static void main(String[] args) {
-        Dictionary dictionary = new Dictionary();
-        System.out.println(dictionary.sortDictionary());
+        buildDictionaryWithMap("Маргарита Заяц Павловна , студентка");
+    }
+
+    public static void buildDictionaryWithMap(String text) {
+        text = text.toLowerCase();
+        text = text.replace("[,.:-?;]", "");
+        String[] split = text.split(" ");
+        LinkedHashMap<String, Integer> map = new LinkedHashMap<>();
+        for (String word : split) {
+            Integer oldCount = map.get(word);
+            if (oldCount == null) {
+                oldCount = 0;
+            }
+            map.put(word, oldCount + 1);
+        }
+        ArrayList<Map.Entry<String, Integer>> entries = new ArrayList<>(map.entrySet());
+        entries.sort((o1, o2) -> Integer.compare(o1.getValue(), o2.getValue()));
+        for (Map.Entry<String, Integer> entry : entries) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
     }
 }
+
